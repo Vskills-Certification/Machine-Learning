c0f8e6e47bb56660ab299b9c1fbf886a3cad1a40
Narender <narender.rana@vskills.in>
2018-09-18 20:33:24 +0530

    Add files via upload

---
+++ 6. Project 2 - SMS Spam Clasifier/sms_spam_classifier.py
@@ -0,0 +1,235 @@
+#Understanding our dataset & data preprocessing
+import pandas as pd
+
+df = pd.read_table("SMSSpamCollection", sep = '\t', header = None,
+     names = ['Label', 'Message'])
+print(df.head())
+
+print(df.shape)
+df['Label'] = df.Label.map({'ham':0, 'spam':1})
+print(df.head())
+
+#Implementing BOW from scratch i.e without using sklearn
+'''converting all strings into lowercase'''
+
+documents = ['Hello, how are you!',
+             'Win money, win from home.',
+             'Call me now.',
+             'Hello, Call hello you tomorrow?']
+
+lower_case_documents = []
+for i in documents:
+    lower_case_documents.append(i.lower())
+print(lower_case_documents)
+
+'''Removing punctuation'''
+
+sans_punctuation_documents = []
+import string
+
+for i in lower_case_documents:
+    sans_punctuation_documents.append(i.translate(str.maketrans('', '', string.punctuation)))
+print(sans_punctuation_documents)
+
+'''Tokenization'''
+
+preprocessed_documents = []
+for i in sans_punctuation_documents:
+    preprocessed_documents.append(i.split(' '))
+print(preprocessed_documents)
+
+'''Count frequencies'''
+
+frequency_list = []
+import pprint
+from collections import Counter
+
+for i in preprocessed_documents:
+    frequency_counts = Counter(i)
+    frequency_list.append(frequency_counts)
+pprint.pprint(frequency_list)
+
+#BOW using sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+count_vector = CountVectorizer()
+print(count_vector)
+count_vector.fit(documents)
+count_vector.get_feature_names()
+
+'''creating a matrix'''
+doc_array = count_vector.transform(documents).toarray()
+print(doc_array)
+
+'''creating a dataframe'''
+frequency_matrix = pd.DataFrame(doc_array,
+                                columns = count_vector.get_feature_names())
+print(frequency_matrix)
+
+
+'''NOTE: sklearn.cross_validation will be deprecated soon to sklearn.model_selection '''
+# Training and Test set
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(df['Message'],
+                                                    df['Label'],
+                                                    random_state=1)
+
+print('Number of rows in the total set: {}'.format(df.shape[0]))
+print('Number of rows in the training set: {}'.format(X_train.shape[0]))
+print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+
+# Instantiate the CountVectorizer method
+count_vector = CountVectorizer()
+
+# Fit the training data and then return the matrix
+training_data = count_vector.fit_transform(X_train)
+
+# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
+testing_data = count_vector.transform(X_test)
+
+#Bayes Theorem from Scratch
+# P(D)
+p_diabetes = 0.01
+
+# P(~D)
+p_no_diabetes = 0.99
+
+# Sensitivity or P(Pos|D)
+p_pos_diabetes = 0.9
+
+# Specificity or P(Neg/~D)
+p_neg_no_diabetes = 0.9
+
+# P(Pos)
+p_pos = (p_diabetes * p_pos_diabetes) + (p_no_diabetes * (1 - p_neg_no_diabetes))
+print('The probability of getting a positive test result P(Pos) is: {}',format(p_pos))
+
+'''
+Instructions:
+Compute the probability of an individual having diabetes, given that, that individual got a positive test result.
+In other words, compute P(D|Pos).
+
+The formula is: P(D|Pos) = (P(D) * P(Pos|D) / P(Pos)
+'''
+
+# P(D|Pos)
+p_diabetes_pos = (p_diabetes * p_pos_diabetes) / p_pos
+print('Probability of an individual having diabetes, given that that individual got a positive test result is:\
+',format(p_diabetes_pos))
+
+'''
+Instructions:
+Compute the probability of an individual not having diabetes, given that, that individual got a positive test result.
+In other words, compute P(~D|Pos).
+
+The formula is: P(~D|Pos) = (P(~D) * P(Pos|~D) / P(Pos)
+
+Note that P(Pos/~D) can be computed as 1 - P(Neg/~D).
+
+Therefore:
+P(Pos/~D) = p_pos_no_diabetes = 1 - 0.9 = 0.1
+'''
+
+
+# P(Pos/~D)
+p_pos_no_diabetes = 0.1
+
+# P(~D|Pos)
+p_no_diabetes_pos = (p_no_diabetes * p_pos_no_diabetes) / p_pos
+print('Probability of an individual not having diabetes, given that that individual got a positive test result is:'\
+,p_no_diabetes_pos)
+
+'''
+Instructions: Compute the probability of the words 'freedom' and 'immigration' being said in a speech, or
+P(F,I).
+
+The first step is multiplying the probabilities of Jill Stein giving a speech with her individual
+probabilities of saying the words 'freedom' and 'immigration'. Store this in a variable called p_j_text
+
+The second step is multiplying the probabilities of Gary Johnson giving a speech with his individual
+probabilities of saying the words 'freedom' and 'immigration'. Store this in a variable called p_g_text
+
+The third step is to add both of these probabilities and you will get P(F,I).
+'''
+
+
+'''
+Solution: Step 1
+'''
+# P(J)
+p_j = 0.5
+
+# P(F/J)
+p_j_f = 0.1
+
+# P(I/J)
+p_j_i = 0.1
+
+p_j_text = p_j * p_j_f * p_j_i
+print(p_j_text)
+
+
+'''
+Solution: Step 2
+'''
+# P(G)
+p_g = 0.5
+
+# P(F/G)
+p_g_f = 0.7
+
+# P(I/G)
+p_g_i = 0.2
+
+p_g_text = p_g * p_g_f * p_g_i
+print(p_g_text)
+
+
+'''
+Solution: Step 3: Compute P(F,I) and store in p_f_i
+'''
+p_f_i = p_j_text + p_g_text
+print('Probability of words freedom and immigration being said are: ', format(p_f_i))
+
+
+'''
+Instructions:
+Compute P(J|F,I) using the formula P(J|F,I) = (P(J) * P(F|J) * P(I|J)) / P(F,I) and store it in a variable p_j_fi
+'''
+
+
+'''
+Solution
+'''
+p_j_fi = p_j_text / p_f_i
+print('The probability of Jill Stein saying the words Freedom and Immigration: ', format(p_j_fi))
+
+
+'''
+Instructions:
+Compute P(G|F,I) using the formula P(G|F,I) = (P(G) * P(F|G) * P(I|G)) / P(F,I) and store it in a variable p_g_fi
+'''
+
+
+'''
+Solution
+'''
+p_g_fi = p_g_text / p_f_i
+print('The probability of Gary Johnson saying the words Freedom and Immigration: ', format(p_g_fi))
+
+
+
+'''Naive bayes using sklearn'''
+
+from sklearn.naive_bayes import MultinomialNB
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+predictions = naive_bayes.predict(testing_data)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
